ec213942d02ba28104f375d778168feea7485b32
Runyu Lu <lry89757@gmail.com>
2023-09-30 20:07:04 +0800

    comment the install cmake

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ options="-DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_INSTALL_PREFIX=install $*"
 ppl_build_dir="${workdir}/ppl-build"
 mkdir ${ppl_build_dir}
 cd ${ppl_build_dir}
-cmd="cmake $options -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. && cmake --build . -j ${processor_num} --config ${build_type} && cmake --build . --target install -j ${processor_num} --config ${build_type}"
+cmd="cmake $options -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. && cmake --build . -j ${processor_num} --config ${build_type}"
+# " && cmake --build . --target install -j ${processor_num} --config ${build_type}"
 echo "cmd -> $cmd"
 eval "$cmd"
